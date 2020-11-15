0626097d0af49e4cde4dfd8fb961edf32815f345
Maurivlz <46494464+Maurivlz@users.noreply.github.com>
2020-11-15 18:17:01 -0300

    update calculator

--- staff/mauri-velez/baby-steps/calculate.js
+++ staff/mauri-velez/baby-steps/calculate.js
@@ -1,2 +1,16 @@
-//TODO hacer la calculadora copadisima
-
+/**
+ * Calculates the result of an operation over two given numbers.
+ *
+ * @example
+ *      calculate(1, '+', 2)
+ *      3
+ *
+ * @param {number} operand1 The first number of the operation.
+ * @param {string} operation The operation to execute (addition '+', subtraction '-', multiplication 'x', division '/').
+ * @param {number} operand2 The second number of the operation.
+ * 
+ * @returns {number} The result of the operation, once applied with the two numbers.
+ */
+function calculate(number1, operation, number2) {
+    // TODO implement me
+}
