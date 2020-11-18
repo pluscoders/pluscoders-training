4fe5dccbaa31cb608cf0ee691f6034bd0d49634c
Maurivelez <74325182+Maurivelez@users.noreply.github.com>
2020-11-18 08:48:03 -0300

    Add files via upload

--- staff/mauri-velez/baby-steps/num-of-chars-without-spaces.js
+++ staff/mauri-velez/baby-steps/num-of-chars-without-spaces.js
@@ -1,7 +1,7 @@
 //next challenge: lo mismo,  pero que no cuente los espacios
 
 function getNumCaracteres(data){
-    var esp = data.replace(/ /g,"", "");
+    var esp = data.replace(/ /g,"");
     return esp.length;
    }
    getNumCaracteres("Manolo me has tenido hasta las 12 de la noche y me sale humito pero lo saque")
