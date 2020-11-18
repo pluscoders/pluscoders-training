62f82afeba526924521d8cf44e87da3f25d28267
Selphyz <47163996+Selphyz@users.noreply.github.com>
2020-11-18 18:14:20 +0100

    return response 200 on connect

--- staff/carlos-dominguez/http-server-node-net/src/index.js
+++ staff/carlos-dominguez/http-server-node-net/src/index.js
@@ -1,27 +1,12 @@
 const net = require("net");
 const server = net.createServer();
-const connectedClients = [];
 server.on("connection", (client) =>{
-  const remote = client.remoteAddress+":"+client.remotePort;
-  console.log("Nueva conexion realizada");
-  connectedClients.push(client);
-  client.on("data", (a)=>{
-    console.log("Data from %s: %s", remote, a);
-    client.write("Hello World! "+d);
-  });
-  client.once("close", ()=>{
-    console.log("Conexion %s: cerrada", remote);
-  });
-  client.on("error", (err)=>{
-    console.log("Connection %s error %s", remote, err.message);
-  });
+  client.end(`HTTP/1.1 200 OK
+Content-Type: text/html; charset=UTF-8
+
+<h1>Hola, Mundo!</h1>
+`);
 });
 server.listen(5000, ()=>{
   console.log("Servidor escuchando en 5000");
 });
-setInterval(() => {
-  const hora = new Date().toISOString();
-  connectedClients.forEach(client => {
-    client.write(hora);
-  });
-}, 2000);
