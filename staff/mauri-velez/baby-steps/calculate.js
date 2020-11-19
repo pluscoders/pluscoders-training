76902f50a1fa4d72b874b53c45e52e32a9d4f9ef
Maurivelez <74325182+Maurivelez@users.noreply.github.com>
2020-11-18 21:32:45 -0300

    Update calculate.js

--- staff/mauri-velez/baby-steps/calculate.js
+++ staff/mauri-velez/baby-steps/calculate.js
@@ -1,27 +1,29 @@
-// // /**
-// //  * Calculates the result of an operation over two given numbers.
-// //  *
-// //  * @example
-// //  *      calculate(1, '+', 2)
-// //  *      3
-// //  *
-// //  * @param {number} operand1 The first number of the operation.
-// //  * @param {string} operation The operation to execute (addition '+', subtraction '-', multiplication 'x', division '/').
-// //  * @param {number} operand2 The second number of the operation.
-// //  * 
-// //  * @returns {number} The result of the operation, once applied with the two numbers.
-// //  */
-// //function calculate(number1, operation, number2) {
-//     // TODO implement me
+/**
+* Calculates the result of an operation over two given numbers.
+*
+* @example
+*      calculate(1, '+', 2)
+*      3
+*
+* @param {number} operand1 The first number of the operation.
+* @param {string} operation The operation to execute (addition '+', subtraction '-', multiplication 'x', division '/').
+* @param {number} operand2 The second number of the operation.
+* 
+* @returns {number} The result of the operation, once applied with the two numbers.
+*/
+function calculate(number1, operation, number2) { 
+    ...
 
 
-function calculate(a, operador, b) {
-    if (operador === '+')
-        return a + b;
-    else if (operador === '-')
-        return a - b;
-    else if (operador === '*')
-        return a * b;
-    else if (operador === '/')
-        return a / b;
-}
+
+    function calculate(a, operador, b) {
+        if (operador === '+')
+            return a + b;
+        else if (operador === '-')
+            return a - b;
+        else if (operador === '*')
+            return a * b;
+        else if (operador === '/')
+            return a / b;
+    }
+
