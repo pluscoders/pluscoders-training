d9d8b63f6d2d9b1b51af2350fc650a2fdb736b35
Maurivlz <46494464+Maurivlz@users.noreply.github.com>
2021-02-09 17:59:28 -0300

    update solution

---
+++ staff/mauri-velez/strings/spell.js
@@ -0,0 +1,7 @@
+///Create a function that spells a text.
+
+Implement a function named spell that receives a text (string) as a parameter and prints each character from that text to the console.//
+
+function spell(text){
+console.log(text.split(''));
+}
