d32d213423779490cc4c8582d0cf0ed30c3557da
Mauri Velez <74325182+Maurivelez@users.noreply.github.com>
2021-02-10 09:21:41 -0300

    Update spell.js

--- staff/mauri-velez/strings/spell.js
+++ staff/mauri-velez/strings/spell.js
@@ -1,7 +1,12 @@
 ///Create a function that spells a text.
 
+///Create a function that spells a text.
+
 Implement a function named spell that receives a text (string) as a parameter and prints each character from that text to the console.//
 
-function spell(text){
-console.log(text.split(''));
-}
+function spell(text){ 
+    for(var i = 0; i < text.length; i++) 
+    console.log(text.charAt(i)); 
+} 
+
+spell("mauri")
