505c0b0a612e46ea5d49141ba8f4bb8ef5a9e52b
Mauri Velez <74325182+Maurivelez@users.noreply.github.com>
2021-02-11 18:21:35 -0300

    Update spell.js
    Manu ahora hice los cambios

--- staff/mauri-velez/strings/spell.js
+++ staff/mauri-velez/strings/spell.js
@@ -4,9 +4,18 @@
 
 Implement a function named spell that receives a text (string) as a parameter and prints each character from that text to the console.//
 
-function spell(text){ 
-    for(var i = 0; i < text.length; i++) 
-    console.log(text.charAt(i)); 
-} 
+// test viejo
+// function spell(text){ 
+//     for(var i = 0; i < text.length; i++) 
+//     console.log(text.charAt(i)); 
+// } 
 
+// spell("mauri")
+
+
+function  spell(text) {
+  var caracter = text.split('');
+  for (var i = 0; i < caracter.length; i++);
+    console.log(caracter[i]);
+}
 spell("mauri")
