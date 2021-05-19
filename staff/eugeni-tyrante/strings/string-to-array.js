a31eac26e4d3fc7160525bcafd7688127c9027d9
Tyrante92 <eugeniomm2@gmail.com>
2021-05-19 22:01:41 +0200

    add string to array

---
+++ staff/eugeni-tyrante/strings/string-to-array.js
@@ -0,0 +1,6 @@
+function stringToArray(text){
+	var res = text.split("");
+  console.log(res);
+}
+
+stringToArray("Hello world")
---
+++ staff/eugeni-tyrante/strings/string-to-array.x.js
@@ -0,0 +1,6 @@
+function stringToArray(text){
+	var res = Array.from(text);
+  console.log(res);
+}
+
+stringToArray("Hello world")
