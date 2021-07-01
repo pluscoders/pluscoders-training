0d6a06d53a21cee85943551c2dc69817600190ed
manuelbarzi <manuelbarzi@gmail.com>
2021-07-01 23:33:19 +0200

    update register-user

--- staff/bruno-diaz/vue-app/logic/register-user.spec.js
+++ staff/bruno-diaz/vue-app/logic/register-user.spec.js
@@ -1,200 +1,230 @@
-describe('registerUser', function () {
-
-    it('should succeed on valid instance with data', function () {
-        var originData = {}
+describe('registerUser', () => {
+    it('should succeed on new user', async () => {
+        const originData = {}
         originData.length = users.length
 
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password')
 
-        // APPLY
-        registerUser(fullname, email, password)
+        await registerUser(fullname, email, password)
 
-        // TEST
-        expect(users.length).toEqual(originData.length + 1)
-
-        var user = users.find(function (user) {
-            return user.fullname === fullname && user.email === email && user.password === password
+        let response = await fetch("https://b00tc4mp.herokuapp.com/api/v2/users/auth", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: `{ "username": "${email}", "password": "${password}" }`,
         })
 
-        expect(fullname).toEqual(user.fullname)
-        expect(email).toEqual(user.email)
-        expect(password).toEqual(user.password)
+        const { token } = await response.json()
+
+        response = await fetch("https://b00tc4mp.herokuapp.com/api/v2/users", {
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+
+        const { fullname: fullname2, username: email2 } = await response.json()
+
+        expect(fullname2).toEqual(fullname)
+        expect(email2).toEqual(email)
     })
 
-    it('should fail on undefined fullname', function() {
-        var fullname
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    fit('should fail when user already exists', async () => {
+        const originData = {}
+        originData.length = users.length
+
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password')
+
+        await fetch("https://b00tc4mp.herokuapp.com/api/v2/users/", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: `{ "fullname": "${fullname}", "username": "${email}", "password": "${password}" }`,
+        })
+
+        let _error
+
+        try {
+            await registerUser(fullname, email, password)
+        } catch(error) {
+            _error = error
+        }
+
+        expect(_error).toBeDefined()
+        expect(_error.message).toEqual(`user with username "${email}" already exists`)
+    })
+
+    it('should fail on undefined fullname', function () {
+        const fullname = undefined
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(TypeError, fullname + ' is not a string')
     })
 
-    it('should fail on undefined email', function() {
-        var fullname = randomString('fullname')
-        var email
-        var password = randomString('password')
+    it('should fail on undefined email', function () {
+        const fullname = getRandomString('fullname')
+        const email = undefined
+        const password = getRandomString('password')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(TypeError, email + ' is not a string')
     })
 
-    it('should fail on invalid email instance: no @', function() {
-        var fullname = randomString('fullname')
-        var email = randomString() + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid email instance: no @', function () {
+        const fullname = getRandomString('fullname')
+        const email = getRandomString() + getRandomString() + '.com'
+        const password = getRandomString('password')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, email + ' is not an e-mail')
     })
 
-    it('should fail on invalid email instance: wrong username', function() {
-        var fullname = randomString('uppercase')
-        var email = '@' + randomString() + '.com'
-        var password = randomString('numeric')
+    it('should fail on invalid email instance: wrong username', function () {
+        const fullname = getRandomString('uppercase')
+        const email = '@' + getRandomString() + '.com'
+        const password = getRandomString('numeric')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, email + ' is not an e-mail')
     })
 
-    it('should fail on invalid email instance: no organitation domain', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + '.com'
-        var password = randomString('password')
+    it('should fail on invalid email instance: no organitation domain', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + '.com'
+        const password = getRandomString('password')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, email + ' is not an e-mail')
     })
 
-    it('should fail on invalid email instance: no extention domain', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString()
-        var password = randomString('password')
+    it('should fail on invalid email instance: no extention domain', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString()
+        const password = getRandomString('password')
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, email + ' is not an e-mail')
     })
 
-    it('should fail on invalid password instance: no password', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password
+    it('should fail on invalid password instance: no password', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = undefined
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(TypeError, password + ' is not a string')
     })
 
-    it('should fail on invalid password instance: no lowcases character', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid password instance: no lowcases character', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        let password = getRandomString('password')
         password = password.slice(password.length - 3, password.length) + password.slice(password.length - 3, password.length)
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should fail on invalid password instance: no uppercase character', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password').toLocaleLowerCase()
+    it('should fail on invalid password instance: no uppercase character', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password').toLocaleLowerCase()
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should fail on invalid password instance: no number', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid password instance: no number', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        let password = getRandomString('password')
         password = password.slice(0, password.length - 2) + password.slice(password.length - 1, password.length)
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should fail on invalid password instance: no symbol', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid password instance: no symbol', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        let password = getRandomString('password')
         password = password.slice(0, password.length - 1)
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should fail on invalid password instance: less than 8 characteres', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid password instance: less than 8 characteres', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        let password = getRandomString('password')
         password = password.slice(password.length - 6, password.length)
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should fail on invalid password instance: more than 13 characteres', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    it('should fail on invalid password instance: more than 13 characteres', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        let password = getRandomString('password')
         password += password + password
 
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname, email, password)
         }).toThrowError(Error, 'The password have to contain between 8 to 13 characters which at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
     })
 
-    it('should succeed width repeat fullname and password', function () {
-        var originData = {}
+    xit('should succeed width repeat fullname and password', () => {
+        const originData = {}
         originData.length = users.length
 
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password')
 
-        var fullnameRepeat = fullname
-        var email2 = randomString() + '@' + randomString() + '.com'
-        var passwordRepeat =  password
+        const fullnameRepeat = fullname
+        const email2 = getRandomString() + '@' + getRandomString() + '.com'
+        const passwordRepeat = password
 
         // APPLY
 
@@ -205,7 +235,7 @@ describe('registerUser', function () {
 
         expect(users.length).toEqual(originData.length + 2)
 
-        var user = users.find(function (user) {
+        const user = users.find(function (user) {
             return user.fullname === fullnameRepeat && user.email === email2 && user.password === passwordRepeat
         })
 
@@ -214,22 +244,22 @@ describe('registerUser', function () {
         expect(passwordRepeat).toEqual(user.password)
     })
 
-    it('should fail with repeat email', function() {
-        var fullname = randomString()
-        var email = randomString() + '@' + randomString() + '.com'
-        var password = randomString('password')
+    xit('should fail with repeat email', function () {
+        const fullname = getRandomString()
+        const email = getRandomString() + '@' + getRandomString() + '.com'
+        const password = getRandomString('password')
 
-        var fullname2 = randomString()
-        var emailRepeat = email
-        var password2 =  randomString('password')
+        const fullname2 = getRandomString()
+        const emailRepeat = email
+        const password2 = getRandomString('password')
 
         // APPLY
 
         registerUser(fullname, email, password)
-        
+
         // APPLY & TEST
 
-        expect(function() {
+        expect(function () {
             registerUser(fullname2, emailRepeat, password2)
         }).toThrowError(Error, 'user already exists')
     })
