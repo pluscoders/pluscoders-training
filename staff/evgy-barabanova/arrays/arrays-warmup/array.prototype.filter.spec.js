95c839ef889ed6a103c9214506c23317a6d9a339
Evgy <evgybarabanova@gmail.com>
2022-01-19 20:36:21 +0300

    implement filter test

---
+++ staff/evgy-barabanova/arrays/arrays-warmup/array.prototype.filter.spec.js
@@ -0,0 +1,15 @@
+describe('Array.prototype.filter', () => {
+  describe('filter things', () => {
+    let nums
+
+    beforeEach(() => {
+      nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    })
+
+    it('should filter numbers that are bigger than 2.5', () => {
+      const res = nums.filter(num => num > 2.5)
+
+      expect(res).toContain(3, 4, 5, 6, 7, 8, 9, 10)
+    })
+  })
+})
