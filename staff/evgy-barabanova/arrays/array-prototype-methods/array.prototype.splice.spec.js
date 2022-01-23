62fa8461b768fe5637d4785c06717b0bcfa93f0e
Evgy <evgybarabanova@gmail.com>
2022-01-23 23:08:07 +0300

    splice

--- staff/evgy-barabanova/arrays/array-prototype-methods/array.prototype.splice.spec.js
+++ staff/evgy-barabanova/arrays/array-prototype-methods/array.prototype.splice.spec.js
@@ -1,7 +1,6 @@
 describe('Array.prototype.splice', () => {
-  describe('splice things', () => {
+  describe('splice numbers', () => {
     let nums
-    let arr = []
 
     beforeEach(() => {
       nums = [1, 2, 3, 4, 5];
@@ -9,17 +8,42 @@ describe('Array.prototype.splice', () => {
 
     it('should new array with splice number ', () => {
       const res = nums.splice(3, 1)
-      arr = res
 
-      expect(res).toBe(arr)
+      expect(res).toHaveLength(1)
+      expect(res).toContain(4)
+      expect(nums).toContain(1, 2, 3, 5)
     })
 
     it('should add element into array', () => {
-      const respon = nums.splice(4, 0, 6)
+      const res = nums.splice(5, 0, 6)
 
-      arr = respon
+      expect(res).toHaveLength(0)
+      expect(nums).toContain(1, 2, 3, 4, 5, 6)
+    })
+  })
 
-      expect(respon).toBe(arr)
+  describe('splice months', () => {
+    let months
+
+    beforeEach(() => {
+      months = ['jan', 'mar', 'apr', 'jun']
+    })
+
+    it('should insert feb between jan and mar', () => {
+      const res = months.splice(1, 0, 'feb')
+
+      expect(res).toHaveLength(0)
+      expect(months).toHaveLength(5)
+      expect(months).toContain('jan', 'feb', 'mar', 'apr', 'jun')
+    })
+
+    it('should replace jun with may', () => {
+      const res = months.splice(3, 1, 'may')
+
+      expect(res).toHaveLength(1)
+      expect(res).toContain('jun')
+      expect(months).toHaveLength(4)
+      expect(months).toContain('jan', 'mar', 'apr', 'may')
     })
   })
 })
