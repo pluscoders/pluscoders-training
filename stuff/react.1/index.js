f36f04652dacfbbd6e4e54c82b1bf315fdf1f598
manuelbarzi <manuelbarzi@gmail.com>
2022-06-01 19:01:36 +0200

    add react demos

---
+++ stuff/react.1/index.js
@@ -0,0 +1,6 @@
+const title = React.createElement('h1', null, 'hola mundo')
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(title)
+
---
+++ stuff/react/index.jsx
@@ -0,0 +1,5 @@
+const title = <h1>hola mundo</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(title)
