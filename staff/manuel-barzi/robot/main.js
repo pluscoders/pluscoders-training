2da16aec075a8cc516029a76b0b533887cc69214
manuelbarzi <manuelbarzi@gmail.com>
2022-07-14 16:22:51 +0200

    update robot; add movement

--- staff/manuel-barzi/robot/main.js
+++ staff/manuel-barzi/robot/main.js
@@ -1,4 +1,32 @@
 var robot = document.querySelector('.robot')
 
-robot.style.left = '300px'
-robot.style.bottom = '500px'
+var x = 200, y = 200
+
+robot.style.left = x + 'px'
+robot.style.bottom = y + 'px'
+
+var step = 20
+
+document.onkeydown = function(event) {
+    var key = event.key
+    
+    console.log(key)
+
+    if (key === 'ArrowUp') {
+        y = y + step
+    
+        robot.style.bottom = y + 'px'
+    } else if(key === 'ArrowDown') {
+        y = y - step
+    
+        robot.style.bottom = y + 'px'
+    } else if(key === 'ArrowRight') {
+        x = x + step
+    
+        robot.style.left = x + 'px'
+    } else if(key === 'ArrowLeft') {
+        x = x - step
+    
+        robot.style.left = x + 'px'
+    }
+}
