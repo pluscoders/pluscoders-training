c37c2c6a8435a77735d10e164e22a741943f3d5f
manuelbarzi <manuelbarzi@gmail.com>
2022-07-21 16:59:00 +0200

    add arrays and strings challenges

---
+++ staff/manuel-barzi/arrays/countStrings.js
@@ -0,0 +1,16 @@
+// HINT see typeof operator
+
+function countStrings(array) {
+    // TODO implement me!
+}
+
+// tests
+
+console.log(countStrings([true, 'one', false, 1, 'two', 2, 3, {}, 'three', function() {}, []]))
+// 3
+
+console.log(countStrings([1, '2', '3', 4, 5, '6', 7, 8, 9, '10']))
+// 4
+
+console.log(countStrings(['1', '2', '3', 4, 5, '6', 7, 8, '9', '10']))
+// 6
