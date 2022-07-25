50f6f5821c0979f50018f481802ad7af12ffa9b8
Ignacio Sanchez <insago@gmail.com>
2022-07-25 19:06:11 +0200

    functional snake without buttons

--- staff/ignacio-sanchez/web/snake/index.jsx
+++ staff/ignacio-sanchez/web/snake/index.jsx
@@ -11,6 +11,8 @@ const STEP = 11
 
 class Snake extends Component {
     constructor() {
+        console.log('Snake -> constructor')
+
         super()
 
         const food = []
@@ -20,102 +22,83 @@ class Snake extends Component {
         food.push(new Bit(Math.round(Math.random() * 10), Math.round(Math.random() * 20)))
 
         this.state = { snake: [new Bit(0, 0)], direction: 'down', food }
-
-        // TODO see Function.prototype.bind() / .call() / .apply()
-        this.handleDownButtonClicked = this.handleDownButtonClicked.bind(this)
-        this.handleRightButtonClicked = this.handleRightButtonClicked.bind(this)
-        this.handleUpButtonClicked = this.handleUpButtonClicked.bind(this)
-        this.handleLeftButtonClicked = this.handleLeftButtonClicked.bind(this)
-    }
-
-    handleKeyPressed(event) {
-        console.log(event)
-    }
-
-    handleDownButtonClicked(event) {
-        const { state: { direction, snake, food } } = this
-
-        if (direction !== 'up') {
-            const preBit = snake[snake.length - 1]
-            const newBit = new Bit(preBit.x, preBit.y + 1)
-
-            const eating = food.some(foodBit => foodBit.x === newBit.x && foodBit.y === newBit.y)
-
-            const newBits = eating? snake.concat(newBit) : snake.slice(1).concat(newBit)
-
-            this.setState({ snake: newBits, direction: 'down' })
-        }
-    }
-
-    handleRightButtonClicked(event) {
-        const { state: { direction, snake, food } } = this
-
-        if (direction !== 'left') {
-            const preBit = snake[snake.length - 1]
-            const newBit = new Bit(preBit.x + 1, preBit.y)
-
-            const eating = food.some(foodBit => foodBit.x === newBit.x && foodBit.y === newBit.y)
-
-            const newBits = eating? snake.concat(newBit) : snake.slice(1).concat(newBit)
-
-            this.setState({ snake: newBits, direction: 'right' })
-        }
-    }
-
-    handleUpButtonClicked(event) {
-        const { state: { direction, snake, food } } = this
-
-        if (direction !== 'down') {
-            const preBit = snake[snake.length - 1]
-            const newBit = new Bit(preBit.x, preBit.y - 1)
-
-            const eating = food.some(foodBit => foodBit.x === newBit.x && foodBit.y === newBit.y)
-
-            const newBits = eating? snake.concat(newBit) : snake.slice(1).concat(newBit)
-
-            this.setState({ snake: newBits, direction: 'up' })
-        }
-    }
-
-    handleLeftButtonClicked(event) {
-        const { state: { direction, snake, food } } = this
-
-        if (direction !== 'right') {
-            const preBit = snake[snake.length - 1]
-            const newBit = new Bit(preBit.x - 1, preBit.y)
-
-            const eating = food.some(foodBit => foodBit.x === newBit.x && foodBit.y === newBit.y)
-
-            const newBits = eating? snake.concat(newBit) : snake.slice(1).concat(newBit)
-
-            this.setState({ snake: newBits, direction: 'left' })
-        }
     }
 
     componentDidMount() {
-        
+        console.log('Snake -> componentDidMount')
+
         setInterval(() => {
-            const { state: { direction } } = this
-            
+            const { state: { snake, food, direction } } = this
+
+            const preBit = snake[snake.length - 1]
+
+            let newBit
+
             switch (direction) {
                 case 'up':
-                    this.handleUpButtonClicked()
-                    break;
+                    newBit = new Bit(preBit.x, preBit.y - 1)
+                    break
+
                 case 'down':
-                    this.handleDownButtonClicked()
-                    break;
-                case 'right':
-                    this.handleRightButtonClicked()
-                    break;
+                    newBit = new Bit(preBit.x, preBit.y + 1)
+                    break
+
                 case 'left':
-                    this.handleLeftButtonClicked()
-                    break;
-            
+                    newBit = new Bit(preBit.x - 1, preBit.y)
+                    break
+
+                case 'right':
+                    newBit = new Bit(preBit.x + 1, preBit.y)
+                    break
+
                 default:
-                    break;
+                    break
             }
 
-        }, 500);
+            const eating = food.some(foodBit => foodBit.x === newBit.x && foodBit.y === newBit.y)
+
+            const newBits = eating ? snake.concat(newBit) : snake.slice(1).concat(newBit)
+
+            this.setState({ snake: newBits })
+        }, 250)
+
+        document.addEventListener('keydown', event => {
+            console.log('document -> keydown')
+
+            const { key } = event
+
+            const { state: { direction } } = this
+
+            let newDirection = direction
+
+            switch (key) {
+                case 'ArrowUp':
+                    if (direction !== 'down')
+                        newDirection = 'up'
+                    break
+
+                case 'ArrowDown':
+                    if (direction !== 'up')
+                        newDirection = 'down'
+                    break
+
+                case 'ArrowLeft':
+                    if (direction !== 'right')
+                        newDirection = 'left'
+                    break
+
+                case 'ArrowRight':
+                    if (direction !== 'left')
+                        newDirection = 'right'
+                    break
+
+                default:
+                    break
+            }
+
+            this.setState({ direction: newDirection })
+        })
+
     }
 
     render() {
@@ -130,13 +113,6 @@ class Snake extends Component {
         })
 
         return <div class="board" >
-            <div className="keyboard">
-                <button onClick={this.handleLeftButtonClicked}>Left</button>
-                <button onClick={this.handleUpButtonClicked}>Up</button>
-                <button onClick={this.handleDownButtonClicked}>Down</button>
-                <button onClick={this.handleRightButtonClicked}>Right</button>
-            </div>
-
             {snakeBits}
             {foodBits}
         </div >
