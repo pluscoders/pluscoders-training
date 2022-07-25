c5987e3bb1367707cb302f345276c16f91428430
Evgy <evgybarabanova@gmail.com>
2022-07-25 18:07:01 +0300

    add design to react

--- staff/evgy-barabanova/stickers/app-react/src/pages/Home.jsx
+++ staff/evgy-barabanova/stickers/app-react/src/pages/Home.jsx
@@ -1,112 +1,137 @@
-import React, { useEffect, useState } from "react"
-import "./Home.css"
+/* eslint-disable no-unreachable */
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 import {
   retrieveUser,
   retrieveNotes,
   createNote,
   updateNote,
   deleteNote,
-} from "../logic"
+} from "../logic";
 
 export default function Home() {
-  const [user, setUser] = useState()
-  const [notes, setNotes] = useState([])
+  const [user, setUser] = useState();
+  const [notes, setNotes] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     try {
       retrieveUser(sessionStorage.token)
         .then((user) => setUser(user))
-        .catch((error) => alert(error.message))
+        .catch((error) => alert(error.message));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }, [])
+  }, []);
+
+  const handlerLogout = () => {
+    delete sessionStorage.token;
+    navigate("/login");
+  };
 
   const handleCreateNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-      createNote(sessionStorage.token, '')
+      createNote(sessionStorage.token, "")
         .then(() => retrieveNotes(sessionStorage.token))
         .then((notes) => setNotes(notes))
-        .catch((error) => alert(error.message))
+        .catch((error) => alert(error.message));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     try {
       retrieveNotes(sessionStorage.token)
         .then((notes) => {
-          setNotes(notes)
+          setNotes(notes);
         })
-        .catch((error) => alert(error.message))
+        .catch((error) => alert(error.message));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }, [])
+  }, []);
 
   const handleDeleteNote = (noteId) => {
     try {
       deleteNote(sessionStorage.token, noteId)
         .then(() => retrieveNotes(sessionStorage.token))
         .then((notes) => setNotes(notes))
-        .catch((error) => alert(error.message))
+        .catch((error) => alert(error.message));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
 
   const handleUpdateNote = (noteId, text) => {
     try {
       updateNote(sessionStorage.token, noteId, text)
         .then(() => retrieveNotes(sessionStorage.token))
         .then((notes) => setNotes(notes))
-        .catch((error) => alert(error.message))
+        .catch((error) => alert(error.message));
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-  }
+  };
 
   return (
     <div>
-      <div>Hello, {user?.name}!</div>
+      <header className="header">
+        <h1 className="header__title">Hello, {user?.name}!</h1>
+        <button
+          className="header__logout-button"
+          onClick={() => handlerLogout()}
+        >
+          Logout
+        </button>
+      </header>
 
-      <h2>Note list</h2>
-      <ul className="noteText">
-        {notes.map((note) => (
-          <div className="textDate">
-            <button onClick={() => handleDeleteNote(note.id)}>Delete</button>
+      <main className="main">
+        <ul className="list">
+          <li className="list__item">
+            {notes.map((note) => (
+              <div className="sticker">
+                <button
+                  className="sticker__delete-button"
+                  onClick={() => handleDeleteNote(note.id)}
+                >
+                  x
+                </button>
 
-            <form
-              onSubmit={(event) => {
-                event.preventDefault()
-                
-                const text = event.target.text.value
-                
-                handleUpdateNote(note.id, text)
-              }}
-              >
-              <textarea
-                name="text"
-                placeholder="Add note"
-                defaultValue={note.text}
-                ></textarea>
-              <button type="submit">Save</button>
-            </form>
-            <div>{note.date}</div>
-          </div>
-        ))}
-      </ul>
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
 
-      <h2>New note</h2>
+                    const text = event.target.text.value;
 
-      <form onSubmit={handleCreateNote}>
-        <button type="submit">+</button>
-      </form>
+                    handleUpdateNote(note.id, text);
+                  }}
+                >
+                  <textarea
+                    className="sticker__text"
+                    name="text"
+                    placeholder="Add note"
+                    defaultValue={note.text}
+                  ></textarea>
+                  <button type="submit">Save</button> 
+                </form>
+                <div>{note.date}</div>
+              </div>
+            ))}
+          </li>
+        </ul>
+
+        <footer className="footer">
+          <form class="footer__add-button" onSubmit={handleCreateNote}>
+            <button type="submit">+</button>
+          </form>
+        </footer>
+      </main>
     </div>
-  )
+  );
 }
 
 // TODO understand "short-circuit evaluation"
--- staff/evgy-barabanova/stickers/doc/README.md
+++ staff/evgy-barabanova/stickers/doc/README.md
@@ -2,3 +2,4 @@ TODO understand and explain BEM
 TODO check my old redux code
 TODO learn defferents between rem and px
 TODO read about seo
+fff
