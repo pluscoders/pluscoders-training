eabed02f6c27b5b6c751c71c21061f6bdbb4ddb0
GravityZ <gravityz231.ba231@gmail.com>
2022-07-27 18:14:52 +0200

    add space ship movement on key space pressed

---
+++ staff/ibra-ba/web/spaceship-3d/_index.js
@@ -0,0 +1,66 @@
+import { GLTFLoader } from "./GLTFLoader.js";
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.01, 1000
+)
+
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+var loader = new GLTFLoader()
+var ship;
+
+loader.load('scene.gltf', function (gltf) {
+    ship = gltf.scene;
+    scene.add(gltf.scene);
+});
+
+scene.background = new THREE.Color(0x200642)
+var light = new THREE.HemisphereLight(0xffffff, 0x000000, 3);
+
+var loader = new THREE.TextureLoader()               // añadi un texture loader para poner una imagen de fondo
+loader.load('spaceback.jpg', function (texture) {
+    scene.background = texture
+})
+
+scene.add(light);
+camera.position.set(0, 100, 300);
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+}
+
+animate();
+
+const step = 10
+
+document.onkeydown = event => {
+    const { key } = event
+
+    //console.log(key)
+
+    if (key === 'ArrowUp')
+        ship.position.y += step
+    else if (key === 'ArrowDown')
+        ship.position.y -= step
+    else if (key === 'ArrowLeft')
+        ship.position.x -= step
+    else if (key === 'ArrowRight')
+        ship.position.x += step
+    else if (key === ' ')
+        launchMissil()
+}
+
+function launchMissil() {
+    let count = 0
+    const intervalId = setInterval(() => {
+        ship.position.y += step
+        count++
+
+        if (count > 9)
+            clearInterval(intervalId)
+    }, 250) // 1s / 4 -> 1000ms / 4 -> 250ms
+}
