c3962cb1ab7731da6dff9ee0f1fbea0d8fccb995
Andrea Altamirano <andrea.altamirano3677@hotmail.com>
2022-08-22 17:25:21 +0200

    adding menu

--- staff/andrea-altamirano/tesab-web/main.js
+++ staff/andrea-altamirano/tesab-web/main.js
@@ -3,12 +3,19 @@ const emailButton = document.querySelector('.email')
 const phoneAnchor = phoneButton.querySelector('a')
 const emailAnchor = emailButton.querySelector('a')
 
-phoneButton.onclick = function(event) {
+phoneButton.onclick = function (event) {
     emailAnchor.classList.add('off')
     phoneAnchor.classList.toggle('off')
 }
 
-emailButton.onclick = function(event) {
+emailButton.onclick = function (event) {
     phoneAnchor.classList.add('off')
     emailAnchor.classList.toggle('off')
+}
+const openMenuButton = document.querySelector('.material-symbols-outlined')
+const closeMenuButton = document.querySelector('.material-symbols-outlined off')
+
+menuButton.onclick = function (event) {
+    openMenuButton.classList.remove('off')
+    closeMenuButton.classList.add('off')
 }
