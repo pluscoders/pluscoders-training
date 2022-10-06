10286e0df7a42c0f0edec714a81e93043da3ae00
Andrea Altamirano <andrea.altamirano3677@hotmail.com>
2022-10-06 17:19:51 +0200

    add tailwind and react to tesab web

---
+++ staff/andrea-altamirano/tesab-web/components/Background.jsx
@@ -0,0 +1,5 @@
+function Background() {
+    return <video autoplay loop muted className="min-w-full min-h-full _max-w-none _max-h-none -z-10 _absolute">
+    <source src="https://tesab-heavy.surge.sh/videos/700ie-drone-video-snippet_bLGz3NnO.mp4" type="video/mp4" />
+</video>
+}
---
+++ staff/andrea-altamirano/tesab-web/components/Header.jsx
@@ -0,0 +1,37 @@
+function Header() {
+    return <header>
+    <div className="flex">
+        <nav className="flex">
+            <a href="https://wa.me/34666967923"><img className="w-9" src="images/social/Wsapp-Blanco.png"
+                alt="WhatsApp" /></a>
+            <a href=""><img className="w-9" src="images/social/Youtube-Blanco.png" alt="YouTube" /></a>
+            <a href="https://www.instagram.com/tesabspain/"><img className="w-9"
+                src="images/social/Instagram-Blanco.png" alt="Instagram" /></a>
+            <a href="https://www.linkedin.com/company/tesab-spain-sl/?viewAsMember=true"><img className="w-9"
+                src="images/social/Linkedin-blanco.png" alt="LinkedIn" /></a>
+            <a href="https://www.facebook.com/tesabspainsl/"><img className="w-9"
+                src="images/social/Facebook-blanco.png" alt="Facebook" /></a>
+        </nav>
+        <nav className="flex">
+            <span>
+                <img className="w-9" src="images/social/Telefono-Blanco.png" alt="Telephone" />
+                <a href="tel:+34944924003">944 924 003</a>
+            </span>
+
+            <span><img className="w-9" src="images/social/Correo-blanco.png" alt="E-mail" />
+                <a href="mailto:tesab@tesabspain.es">tesab@tesabspain.es</a></span>
+        </nav>
+    </div>
+
+    <div>
+        <h1>Tesab Spain</h1>
+
+        <div>
+            <img src="images/tesab-logo.png" alt="" />
+            <h2>Alquiler y venta de maquinaria</h2>
+        </div>
+
+        <span>menu</span>
+    </div>
+</header>
+}
