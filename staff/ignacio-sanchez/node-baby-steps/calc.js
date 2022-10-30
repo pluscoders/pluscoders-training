ea43a70fcf011c6ddc52f53e98250387b0a72fcf
Ignacio Sanchez <insago@gmail.com>
2022-10-30 11:54:59 +0100

    new hooks on snake and node

---
+++ staff/ignacio-sanchez/node-baby-steps/calc.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates basic math operations
+ * 
+ * Example:
+ * $ node calc add 10 10
+ * // 20
+ * $ node calc mul 10 10
+ * // 100
+ * $ node calc div 10  10
+ * // 1
+ * $ node calc sub 10 10
+ * // 0
+ */
